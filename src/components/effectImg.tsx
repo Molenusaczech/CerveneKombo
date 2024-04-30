aa84e965a0b2a3cdcb3033c5a21d8e24e671eae9
Molenusaczech <06krtek@gmail.com>
2024-04-30 18:29:55 +0200

    implement icons

---
+++ src/components/effectImg.tsx
@@ -0,0 +1,37 @@
+import { effectColor, effectName } from "@/types/effects";
+
+const colorClasses: Record<effectColor, string> = {
+    "stop": "R",
+    "opt": "O",
+    "go": "G"
+}
+
+const effectClasses: Record<effectName, string> = {
+    "all": "bless",
+    "combo": "combo",
+    "combo-trigger": "combo-detonator",
+    "block": "defense",
+    "add-energy": "energy",
+    "heal": "heal",
+    "hit-hero": "hero-attack",
+    "hit-hero-repeated": "permanent-attack",
+    "hit-anything": "universal-attack",
+    "hit-weapon": "weapon-attack",
+    "crunch-weapon": "obliterate",
+    "stop-discard": "battle-trick",
+    "curse-weapon": "curse-weapon",
+    "night-attack": "night-attack",
+    "vampire-bite": "vampire-bite",
+    "fix-weapon": "mechanics-superaction",
+    "hit-double": "zepplandian-shrapnel",
+    "trap": "trap",
+    "cumulative-attack": "cumulus",
+    "extra-coins": "extra-coins"
+}
+
+export default function EffectImg(props: { effect: effectName, color: effectColor }) {
+    const { effect, color } = props;
+    return (
+        <span className={"icon-32 action-type "+effectClasses[effect]+" "+colorClasses[color]}></span>
+    )
+}
--- src/components/type/typeInfo.tsx
+++ src/components/type/typeInfo.tsx
@@ -1,10 +1,14 @@
 import { heroCid } from "@/data/heroTypeData";
 import { weaponCid } from "@/data/weaponTypeData";
+import EffectImg from "../effectImg";
 
 export default function TypeInfo(props: { cid: heroCid | weaponCid }) {
   return (
     <div>
       Typ: {props.cid}
+      <EffectImg effect="all" color="go" />
+      <EffectImg effect="combo" color="opt" />
+      <EffectImg effect="combo-trigger" color="stop" />
     </div>
   )
 }
