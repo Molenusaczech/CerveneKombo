dc8cc23f7f7706f68409e174e6520d40f9f07ce9
Molenusaczech <06krtek@gmail.com>
2024-05-09 17:18:39 +0200

    add keys

--- src/components/replay/replayView.tsx
+++ src/components/replay/replayView.tsx
@@ -19,20 +19,28 @@ export default function ReplayView(props: {
 
     return (
         <div className="grid grid-cols-6">
-            
-        <Card data={decks[0].hero} height={400}/>
 
-        {decks[0].weapons.map((weapon) => {
-            return <Card data={weapon} height={300}/>
+            <Card data={decks[0].hero} height={400} />
 
-        })}
+            {decks[0].weapons.map((weapon, index) => {
+                return (
+                    <div key={index}>
+                        <Card data={weapon} height={300} />
+                    </div>
+                )
 
-        <Card data={decks[1].hero} height={400}/>
-            
-        {decks[1].weapons.map((weapon) => {
-            return <Card data={weapon} height={300}/>
+            })}
 
-        })}
+            <Card data={decks[1].hero} height={400} />
+
+            {decks[1].weapons.map((weapon, index) => {
+                return (
+                    <div key={index}>
+                        <Card data={weapon} height={300} />
+                    </div>
+                )
+
+            })}
 
         </div>
     )
