3f45145d6acba709659b7dcab7744188c41f5bbb
Molenusaczech <06krtek@gmail.com>
2024-07-19 15:35:47 +0200

    tournament page scraping finished

--- tools/tournament/getTournamentData.js
+++ tools/tournament/getTournamentData.js
@@ -1,8 +1,9 @@
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom
+const fs = require("fs")
 global.DOMParser = new JSDOM().window.DOMParser
 
-const data = {};
+const data = [];
 
 async function scrapeTournament(url) {
     console.log("Scraping tournament: " + url);
@@ -20,7 +21,7 @@ async function scrapeTournament(url) {
     console.log(rows.length);
 
     let tournamentData = {
-        id: url.split("/")[3],
+        id: url.split("/")[5],
         name: doc.querySelector("h1").textContent.trim(),
         link: url,
         matches: []
@@ -38,8 +39,18 @@ async function scrapeTournament(url) {
             const href = link.getAttribute("href");
             console.log(href);
 
+
             // 0 = draw, 1 = player1, 2 = player2
-            let winner = 0;
+
+            let match = {
+                id: null,
+                link: null,
+                players: [],
+                winner: 0
+            }
+
+            match.id = href.split("/")[3];
+            match.link = href;
 
             [1, 2].forEach(playerIndex => {
 
@@ -50,15 +61,27 @@ async function scrapeTournament(url) {
 
                 if (name.includes("🏆")) {
                     isWinner = true;
-                    winner = playerIndex;
+                    match.winner = playerIndex;
                     name = name.replace("🏆", "");
                     name = name.trim();
                 }
                 console.log(name + id + isWinner);
+
+                match.players.push(name + id);
             })
+
+            tournamentData.matches.push(match);
 
         }
     });
+
+    tournamentData.matches.sort((a, b) => {
+        return a.id - b.id;
+    });
+
+    data.push(tournamentData);
+
+    fs.writeFileSync("src/data/raw/tournaments/rawTournamentData.json", JSON.stringify(data, null, 4));
 }
 
 scrapeTournament("https://scratchwars-online.cz/cs/tournaments/swo-ligovy-turnaj-16-7-2024-168/");
